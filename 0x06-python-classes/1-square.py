b3009ab2c0e0d5c850d639a7acfd5f2a757f0d43
millyanne93 <nmillyanne20@gmail.com>
2023-10-24 06:43:25 -0700

    readme file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """This class defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size):
+        """
+        The initialization function for the square class.
+        """
+        self.__size = size
