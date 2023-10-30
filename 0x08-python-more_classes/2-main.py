666d8cfc6c40147b05e0a239279b99a32ccd4e4c
millyanne93 <nmillyanne20@gmail.com>
2023-10-30 03:06:14 -0700

    3-rectangle.py

--- 0x08-python-more_classes/2-main.py
+++ 0x08-python-more_classes/2-main.py
@@ -9,8 +9,3 @@ print("--")
 my_rectangle.width = 10
 my_rectangle.height = 3
 print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-guillaume@ubuntu:~/0x08$ ./2-main.py
-Area: 8 - Perimeter: 12
---
-Area: 30 - Perimeter: 26
