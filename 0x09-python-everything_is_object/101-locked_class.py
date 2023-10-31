db134a631a4b401a3576c75449a336e44b0f2d30
millyanne93 <nmillyanne20@gmail.com>
2023-10-31 08:09:26 -0700

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Defines a locked class with limited attribute access."""
+
+
+class LockedClass:
+    """Restricts new attributes to 'first_name' only."""
+    
+    __slots__ = ["first_name"]
