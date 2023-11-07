5fa86f4cc29afac12da170a545a9ccf12c0ef1b9
millyanne93 <nmillyanne20@gmail.com>
2023-11-07 09:57:27 -0800

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Function that defines the class Student
+"""
+
+
+class Student:
+    """ Class to create student instances """
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
