de26df0693713491a31f7274020de138ec2f7181
millyanne93 <nmillyanne20@gmail.com>
2023-11-07 12:13:04 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 import sys
 
 if __name__ == "__main__":
