35d4082d6423a436a4c89b3ea30c6ecbb15f60af
millyanne93 <nmillyanne20@gmail.com>
2024-01-25 06:19:10 -0800

    fix errors

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Send a GET request with a custom header to the URL and display the body of the response
+# Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl "$1" -sX GET -H "X-School-User-Id: 98"
-
