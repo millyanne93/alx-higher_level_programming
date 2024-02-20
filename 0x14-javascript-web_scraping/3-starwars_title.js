8fad2d9286f9e020a627da514148fc64cbe5ea37
millyanne93 <nmillyanne20@gmail.com>
2024-02-20 11:33:46 -0800

    add solution

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  
+  if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  console.log(movie.title);
+});
